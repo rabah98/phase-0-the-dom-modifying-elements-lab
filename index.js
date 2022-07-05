33c455b8b5a9151e03584d7d913b367941566104
Rabah <rmerzoug471@gmail.com>
2022-07-04 23:29:49 -0500

    commit tree b946b83f8938727ead95c283a1c6ca76c7c4a483

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+const body = document.querySelector('body');
+body.append(newHeader);
